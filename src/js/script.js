f147812c75a7d611f1298d359f527c0e5d1c9b03
Lucie-Garcia <lucie.garcia05@edu.univ-fcomte.fr>
2025-12-03 14:13:24 +0100

    add code js & css

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
+const lightbox = document.querySelector("#lightbox");
+
+const lightboxImage = document.querySelector("#lightbox img");
+
+lightbox?.addEventListener("click", () => lightbox.close());
+document.body.addEventListener("click", (evt) => {
+    if (!evt.target.matches("[data-full-img]")) return;
+    lightboxImage.src = evt.target.dataset.fullImg;
+    lightbox.showModal();
+});
 // *** LIGHTBOX ***
